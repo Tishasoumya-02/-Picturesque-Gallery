2f6e6ed75e032c4308abec9966e492cc54c76200
Salokya Kumar <kumar.salokya007@gmail.com>
2021-10-02 23:24:22 +0530

    added gradient

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -8,14 +8,10 @@ import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
 
 const BarStyle = makeStyles({
   root: {
-    background: '#EC6F66',                                             /* fallback for old browsers */
-    // eslint-disable-next-line no-dupe-keys
-    background: '-webkit-linear-gradient(to right, #F3A183, #EC6F66)', /* Chrome 10-25, Safari 5.1-6 */
-    // eslint-disable-next-line no-dupe-keys
-    background: 'linear-gradient(to right, #F3A183, #EC6F66)',         /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
+      background: 'FBDA61',
+      backgroundImage: 'linear-gradient(45deg, #FBDA61 0%, #FF5ACD 100%)',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
   },
 });
 
